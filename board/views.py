08b6cdc35a50374f787624d46d6c637cc5625cad
tlsalsckd13@naver.com <root@ip-172-16-0-175.ap-northeast-2.compute.internal>
2024-08-20 13:36:47 +0000

    Initial commit

---
+++ board/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'board/index.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'board/post_detail.html', {'post': post})
