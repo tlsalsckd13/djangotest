5c689005910689418bd9f396ba17d54e1ee07a86
tlsalsckd13 <root@ip-172-16-0-175.ap-northeast-2.compute.internal>
2024-08-20 14:28:03 +0000

    Edit view

--- board/views.py
+++ board/views.py
@@ -3,8 +3,8 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.all()
-    return render(request, 'board/index.html', {'posts': posts})
+    return render(request, 'board/index.html', {'posts-test': posts})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'board/post_detail.html', {'post': post})
+    return render(request, 'board/post_detail.html', {'post-test': post})
